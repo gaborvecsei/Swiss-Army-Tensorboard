644712d654fd85a1e363567dbd087a2b3587788e
Gábor Vecsei <vecseigabor.x@gmail.com>
2018-10-15 09:18:51 +0200

    init

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='swiss_army_tensorboard',
+    version='0.0.1',
+    url='gaborvecsei.com',
+    author='Gabor Vecsei',
+    packages=find_packages(),
+)
---
+++ swiss_army_tensorboard/__init__.py
@@ -0,0 +1,1 @@
+from swiss_army_tensorboard.tfboard import tfboard_loggers
